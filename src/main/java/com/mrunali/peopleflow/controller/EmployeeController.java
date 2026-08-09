2cdb51011d7e87bab0ce610691d1b63fe0954203
Mrunali Pusadkar <mrunalipusadkar@gmail.com>
2026-08-09 17:48:21 +0530

    Complete Employee CRUD API

--- src/main/java/com/mrunali/peopleflow/controller/EmployeeController.java
+++ src/main/java/com/mrunali/peopleflow/controller/EmployeeController.java
@@ -14,4 +14,31 @@ public class EmployeeController {
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+
+        return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@RequestBody Employee employee,
+                                   @PathVariable Long id) {
+        return employeeService.updateEmployee(employee, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+    }
 }
